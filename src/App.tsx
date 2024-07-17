6203706df8364704478b8289ddb74aef7dc21c2c
kaizhou.chen <chenkaizhou@outlook.com>
2024-07-17 17:11:30 +0800

    antd切换为中文

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
+import React, { useCallback, useEffect, useState } from 'react'
 import { RouterProvider } from 'react-router-dom'
 import router from '@/router/router'
 
+import { ConfigProvider } from 'antd'
+import zhCN from 'antd/locale/zh_CN';
+
+import dayjs from 'dayjs';
+import 'dayjs/locale/zh-cn';
+
 function App() {
+  const [locale, setLocale] = useState(zhCN)
+
+  useEffect(() => {
+    dayjs.locale('zh-cn');
+  }, [])
+
   return (
-    <RouterProvider router={router}></RouterProvider>
+    <ConfigProvider locale={locale}>
+      <RouterProvider router={router}></RouterProvider>
+    </ConfigProvider>
   )
 }
 
